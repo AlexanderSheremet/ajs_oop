1ef003cd3209d9b9eb370d9da1b743f361ccd31d
Sheremet1 <eparis@atlassian.com>
2023-08-10 22:59:23 +0300

    change

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,59 +1,46 @@
 export default class Character {
-  constructor(name, type, attack, defence) {
+  constructor(name, type) {
+    const types = [
+      'Bowerman',
+      'Swordsman',
+      'Magician',
+      'Daemon',
+      'Undead',
+      'Zombie',
+    ];
+
+    if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+      throw new Error('Передано некорректное значение имени персонажа');
+    }
+    if (!types.includes(type)) {
+      throw new Error('Передано некорректное значение типа персонажа');
+    }
+
     this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = attack;
-    this.defence = defence;
-  }
-
-  get name() {
-    return this.name;
-  }
-
-  set name(value) {
-    if (value.length < 2 || value.length > 10) {
-      throw new Error('Некорректная длина строки'); //
-    }
-    this.name = value;
-  }
-
-  get type() {
-    return this.type;
-  }
-
-  set type(value) {
-    if (
-      ![
-        'Bowman',
-        'Swordsman',
-        'Magician',
-        'Daemon',
-        'Undead',
-        'Zombie',
-      ].includes(value)
-    ) {
-      throw new Error('Некорректный тип игрока');
-    }
-    this.type = value;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 
   levelUp() {
-    if (this.health !== 0) {
-      this.level += 1;
-      this.attack *= 1.2;
-      this.defence *= 1.2;
-      this.health = 100;
-    } else {
+    if (this.health <= 0) {
       throw new Error('Нельзя повысить уровень умершего');
     }
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
   }
 
   damage(points) {
-    if (this.health < 0) {
-      throw new Error('Здоровье ниже нуля');
+    if (this.health <= 0) {
+      return;
     }
     this.health -= points * (1 - this.defence / 100);
+    if (this.health < 0) {
+      this.health = 0;
+    }
   }
 }
